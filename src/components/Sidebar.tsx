34c6b25ebde07b9beb0f615406de0f3138d47cb4
Kartikay Thakur <sky.kartikay@gmail.com>
2025-10-19 00:10:35 +0530

    logo fixes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar({ materials, selectedMaterials, onFileSelect, on
     <aside className="w-full lg:w-1/3 xl:w-1/4 p-6 bg-white border-[1.5pt] border-black flex flex-col"> 
       <div className='my-4'></div>
       <h1 className="text-2xl text-gray-900 mb-1  font-mono ">
-        <div className='text-4xl text-blue-500'><img src="../../my-logo.png" alt="P4P Logo" className="h-14  bg-black rounded-full p-1 mb-4 mx-4"/>@plan<span className='text-red-500'>4</span>prepp.</div> </h1>
+        <div className='text-4xl text-blue-500'><img src="/my-logo.png" alt="P4P Logo" className="h-14   bg-black rounded-full p-1 mb-4 mx-4"/>@plan<span className='text-red-500'>4</span>prepp.</div> </h1>
       <p className="text-gray-500 font-mono mb-6">Load, select, and test your knowledge.</p>
 
       <div className="mb-6">
